ba0631c25f20db470f3fc9d079093e17f0d52849
Cédric Michel <c-e-d@live.fr>
2022-11-03 23:48:24 +0100

    ajout du plugin + configurations

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSortDown, faSortUp } from '@fortawesome/free-solid-svg-icons'
+import './styles.module.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export const Dropdown = ({ list, setValue }) => {
+  const [open, setOpen] = useState(false)
+  const [selectedValue, setSelectedValue] = useState()
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const toogleDropdown = () => {
+    setOpen(!open)
+  }
+
+  const data = []
+
+  useEffect(() => {
+    if (selectedValue === undefined) {
+      setSelectedValue(data[0].value)
+    }
+    setValue(selectedValue)
+  }, [selectedValue, setValue, data])
+
+  for (let i = 0; i < list.length; i++) {
+    const dataDropdown = {
+      index: i,
+      value: list[i]
+    }
+    data.push(dataDropdown)
+  }
+
+  const selectLi = (e) => {
+    setOpen(false)
+    setSelectedValue(e.value)
+  }
+
+  return (
+    <div className='dropdown' onClick={toogleDropdown}>
+      <div className='list'>
+        <div className='list_el'>{selectedValue}</div>
+        {open && (
+          <ul className='listUl'>
+            {data.map((data) => (
+              <li
+                className='listLi'
+                key={data.index}
+                onClick={() => selectLi(data)}
+              >
+                {data.value}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      {open ? (
+        <FontAwesomeIcon className='arrow' icon={faSortUp} alt='up arrow' />
+      ) : (
+        <FontAwesomeIcon className='arrow' icon={faSortDown} alt='down arrow' />
+      )}
+    </div>
+  )
+}
